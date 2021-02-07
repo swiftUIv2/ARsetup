426084ba1329a464a8c24906a1492b1168900895
emm <723125@gmail.com>
2021-02-07 11:49:28 +0000

    Initial Commit

---
+++ ARF/ARFApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARFApp.swift
+//  ARF
+//
+//  Created by emm on 07/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct ARFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ARF/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ARF
+//
+//  Created by emm on 07/02/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
